48c02c4f8ea75a0cbfe6496d45d03a1f5b42ba01
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2025-04-28 21:49:35 +0800

    Update Lexer.java

--- Lexer.java
+++ Lexer.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 public class Lexer {
     private ArrayList<Token> tokens = new ArrayList<Token>();
     private String line;
+    private Type type = Type.READ;
     
     public Lexer(String in) {
         this.line = in;
@@ -11,7 +12,45 @@ public class Lexer {
 
     public ArrayList<Token> tokenize() {
         for (char c : line.toCharArray()) {
-            if (
+            switch (type) {
+                case Type.READ:
+                    if (c == ' ') continue;
+                    else if (isNumber(c)) {
+                        data += c;
+                        type = Type.NUMBER;
+                    } else if (isOperator(c)) {
+                        data += c;
+                        type = Type.OPERATOR;
+                    } else if (isSymbol(c)) {
+                        data += c;
+                        type = Type.SYMBOL;
+                    } else if (c == '\n') {
+                        data = "";
+                        type = Type.EOF;
+                    } else {
+                        data += c;
+                        type = Type.ERROR;
+                    }
+                    break;
+                case Type.NUMBER:
+                    break;
+                case Type.OPERATOR:
+                    break;
+                case Type.SYMBOL:
+                    break;
+                case Type.EOF:
+                    break;
+                case Type.ERROR:
+                    if (c == '\n') {
+                        tokens.add(new Token(Type.ERROR, data));
+                        data = "";
+                        type = Type.EOF;
+                    } else {
+                        data += c;
+                        type = Type.ERROR;
+                    }
+                    break;
+            }
         }
         return tokens;
     }
