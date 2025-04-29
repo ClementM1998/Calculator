5fb81738bfcd5764ce1bedc67edad193fbd466d3
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2025-04-29 08:56:09 +0800

    Update Arithmetic.java

--- Arithmetic.java
+++ Arithmetic.java
@@ -1,7 +1,15 @@
 
 public class Arithmetic {
+    private Object result;
+    private Object left;
+    private Object right;
+    private String operator;
 
     public Arithmetic(Object left, String operator, Object right) {
+        this.left = left;
+        this.operator = operator;
+        this.right = right;
+        this.result = null;
     }
 
     
