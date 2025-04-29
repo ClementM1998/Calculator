e80d6442e07a2cd399fdf3ca14b2b2d9b1cc4efd
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2025-04-29 08:29:53 -0400

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 public class Calculator {
@@ -12,10 +11,24 @@ public class Calculator {
             System.out.print("Calculator > ");
             String in = scanner.nextLine().toLowerCase();
             if (in.equals("exit")) break;
+            else if (in.equals("clear")) {
+                String os = System.getProperty("os.name").toLowerCase();
+                try {
+                    if (os.contains("win")) {
+                        // Untuk Windows
+                        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+                    } else {
+                        // Untuk Linux dan Mac
+                        new ProcessBuilder("clear").inheritIO().start().waitFor();
+                    }
+                } catch (Exception e) {
+                    System.out.println("Screen can't clear");
+                }
+            }
             parser = new Parser(lexer.tokenize(in));
             Object value = parser.execute();
             if (value != null) System.out.println(value + " #");
         }
-        
+
     }
 }
