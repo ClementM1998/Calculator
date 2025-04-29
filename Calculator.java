c4c4769178b80f7a1d7c4ff8e44379979ab2f60d
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2025-04-29 08:17:09 -0400

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,20 @@
 
-public class Calculator {
+import java.util.Scanner;
 
-    
+public class Calculator {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Lexer lexer = new Lexer();
+        Parser parser;
+
+        while (true) {
+            System.out.print("Calculator > ");
+            String in = scanner.nextLine();
+            parser = new Parser(lexer.tokenize(in));
+            Object value = parser.execute();
+            if (value != null) System.out.println(value + " #");
+        }
+        
+    }
 }
